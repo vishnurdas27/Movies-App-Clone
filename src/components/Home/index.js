7e5ef9a9fad70cfa9098fac19001c8a27632a7a8
vishnurdas27 <vishnurdas27@gmail.com>
2026-02-17 10:56:47 +0000

    home page creating

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -5,6 +5,14 @@ const Home = () => (
   <div className="main-container">
     <div className="Hero-container">
       <Navbar />
+      <div className="hero-section">
+        <h1 className="hero-title">Super Man</h1>
+        <p className="hero-desc">
+          Superman is a fictional superhero who first appeared in American Comic
+          Books published by DC Comics
+        </p>
+        <button className="play-btn">Play</button>
+      </div>
     </div>
   </div>
 )
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -3,44 +3,44 @@ import {HiOutlineSearch} from 'react-icons/hi'
 import './index.css'
 
 const Navbar = () => (
-  <nav className='navbar-container'>
-    <div className='logo-container'>
-      <Link to='/'>
+  <nav className="navbar-container">
+    <div className="logo-container">
+      <Link to="/">
         <img
-          src='https://res.cloudinary.com/dfthypiat/image/upload/v1771312433/Group_7399_ajucnq.png'
-          className='logo'
-          alt='website logo'
+          src="https://res.cloudinary.com/dfthypiat/image/upload/v1771312433/Group_7399_ajucnq.png"
+          className="logo"
+          alt="website logo"
         />
       </Link>
     </div>
 
-    <div className='sub-container'>
-      <div className='features'>
-        <ul className='nav-links-list'>
+    <div className="sub-container">
+      <div className="features">
+        <ul className="nav-links-list">
           <li>
-            <Link to='/' className='nav-link'>
+            <Link to="/" className="nav-link">
               Home
             </Link>
           </li>
           <li>
-            <Link to='/popular' className='nav-link'>
+            <Link to="/popular" className="nav-link">
               Popular
             </Link>
           </li>
         </ul>
 
-        <div className='actions-container'>
-          <Link to='/search'>
-            <button type='button' className='search-button'>
-              <HiOutlineSearch className='search-icon' />
+        <div className="actions-container">
+          <Link to="/search">
+            <button type="button" className="search-button">
+              <HiOutlineSearch className="search-icon" />
             </button>
           </Link>
 
-          <Link to='/account'>
+          <Link to="/account">
             <img
-              src='https://res.cloudinary.com/dfthypiat/image/upload/v1771318618/Avatar_hdmewo.png'
-              className='Avatar'
-              alt='profile'
+              src="https://res.cloudinary.com/dfthypiat/image/upload/v1771318618/Avatar_hdmewo.png"
+              className="Avatar"
+              alt="profile"
             />
           </Link>
         </div>
